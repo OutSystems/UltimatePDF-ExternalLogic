ae3c66d820d3faa39c4f1165c9fa60c684911c86
Diogo Paulo <diogo.paulo@outsystems.com>
2024-07-04 11:23:11 +0100

    Remove the zip package if exists
    Remove the zip package if exists

--- generate_upload_package.ps1
+++ generate_upload_package.ps1
@@ -1,3 +1,4 @@
+Remove-Item -Path .\UltimatePDF_ExternalLogic.zip -Force
 Set-ExecutionPolicy -Scope CurrentUser Unrestricted
 dotnet publish -c Release -r linux-x64 --self-contained false
 Compress-Archive -Path .\UltimatePDF_ExternalLogic\bin\Release\net6.0\linux-x64\publish\* -Update -DestinationPath UltimatePDF_ExternalLogic.zip
