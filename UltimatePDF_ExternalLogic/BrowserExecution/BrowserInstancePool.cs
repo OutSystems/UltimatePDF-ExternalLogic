602aaad99149a351b9b42fe198ccfa9113757e9a
Diogo Paulo <diogo.paulo@outsystems.com>
2024-07-04 11:56:42 +0100

    Added Event Handlers
    Added event handlers to the browser logger to log Errors, Page Errors, and Console events

--- UltimatePDF_ExternalLogic/BrowserExecution/BrowserInstancePool.cs
+++ UltimatePDF_ExternalLogic/BrowserExecution/BrowserInstancePool.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using HeadlessChromium.Puppeteer.Lambda.Dotnet;
 using Microsoft.Extensions.Logging;
 using OutSystems.UltimatePDF_ExternalLogic.Management.Troubleshooting;
+using OutSystems.UltimatePDF_ExternalLogic.Utils;
 using PuppeteerSharp;
+using UltimatePDF_ExternalLogic.Utils;
 
 namespace OutSystems.UltimatePDF_ExternalLogic.BrowserExecution {
     public class BrowserInstancePool {
@@ -22,7 +26,7 @@ namespace OutSystems.UltimatePDF_ExternalLogic.BrowserExecution {
             try {
                 var instance = pool.FirstOrDefault(i => i.IsHealthy);
 
-                if(instance == null) {
+                if (instance == null) {
                     logger.Log("Create new Browser Instance");
 
                     var browserLauncher = new HeadlessChromiumPuppeteerLauncher(logger.GetLoggerFactory("browser.txt"));
@@ -40,7 +44,36 @@ namespace OutSystems.UltimatePDF_ExternalLogic.BrowserExecution {
         public async Task<PooledPage> NewPooledPage(Logger logger) {
             var instance = await NewBrowserInstance(logger);
             var page = await instance.Browser.NewPageAsync();
-            return new PooledPage(page, logger);
+            var pooledPage = new PooledPage(page, logger);
+            RegisterPageEventHandlers(logger, pooledPage);
+            return pooledPage;
+        }
+
+        private static void RegisterPageEventHandlers(Logger logger, PooledPage page) {
+            RegisterPageErrorHandler(logger, page);
+            RegisterErrorHandler(logger, page);
+            RegisterConsoleHandler(logger, page);
+        }
+
+        private static void RegisterConsoleHandler(Logger logger, PooledPage page) {
+            page.Page.Console += (sender, e) => {
+                logger.Log($"Console - ");
+                for (var i = 0; i < e.Message.Args.Count; i++) {
+                    logger.Log($"\t[{i}]: {e.Message.Args[i]}");
+                }
+            };
+        }
+
+        private static void RegisterErrorHandler(Logger logger, PooledPage page) {
+            page.Page.Error += (o, e) => {
+                logger.Error($"Error Event - {e.Error}");
+            };
+        }
+
+        private static void RegisterPageErrorHandler(Logger logger, PooledPage page) {
+            page.Page.PageError += (o, e) => {
+                logger.Error($"Page Error Event - {e.Message}");
+            };
         }
     }
 }
