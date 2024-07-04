e282dddd3faaba2bc66f4cfafa07834584ca8266
Diogo Paulo <diogo.paulo@outsystems.com>
2024-07-04 11:22:05 +0100

    Updated the TestLambda project
    * improved the readme file
    * create a simpler call to puppeteer without the extra ultimate pdf custom code

--- TestLambda_UltimatePDF_ExternalLogic/src/TestLambda_UltimatePDF_ExternalLogic/Function.cs
+++ TestLambda_UltimatePDF_ExternalLogic/src/TestLambda_UltimatePDF_ExternalLogic/Function.cs
@@ -4,6 +4,10 @@ using Amazon.Lambda.Core;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Model.Internal.MarshallTransformations;
+using HeadlessChromium.Puppeteer.Lambda.Dotnet;
+using OutSystems.UltimatePDF_ExternalLogic.Management.Troubleshooting;
+using OutSystems.UltimatePDF_ExternalLogic.Utils;
+using PuppeteerSharp;
 using ExternalLogic = OutSystems.UltimatePDF_ExternalLogic;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -22,10 +26,40 @@ public class Function {
         var url = input.Url;
         var bucket = input.Bucket;
 
+        var (pdf, logsZipFile) = Call_UltimatePDF_ExternalLogic(url);
+        //var (pdf, logsZipFile) = AsyncUtils.StartAndWait(() => Call_BasicPuppeteer(url));
+
+        var timestamp = DateTime.Now.ToString("yyyyMMddTHHmmssfff");
+        Task[] tasks = {
+            Task.Run(() => PutS3Object(bucket, $"pdf_{timestamp}.pdf", pdf)),
+            Task.Run(() => PutS3Object(bucket, $"logs_{timestamp}.zip", logsZipFile))
+        };
+        Task.WaitAll(tasks);
+
+        return $"Got a pdf with size {((pdf.Length / 1024.0f) / 1024.0f)} Mb and the logs as {((logsZipFile.Length / 1024.0f) / 1024.0f)} Mb";
+    }
+
+    private async Task<(byte[] pdf, byte[] logsZipFile)> Call_BasicPuppeteer(string url) {
+        byte[] pdf;
+        var logger = Logger.GetLogger(true, true);
+        var browserLauncher = new HeadlessChromiumPuppeteerLauncher(logger.GetLoggerFactory("browser.txt"));
+
+        using (var browser = await browserLauncher.LaunchAsync())
+        using (var page = await browser.NewPageAsync()) {
+            await page.GoToAsync(url);
+            await page.WaitForSelectorAsync(":root:not(.ultimate-pdf-is-not-ready)");
+            pdf = await page.PdfDataAsync();
+        }
+
+        return (pdf, logger.GetZipFile());
+    }
+
+    private static (byte[], byte[]) Call_UltimatePDF_ExternalLogic(string url) {
+        var uri = new Uri(url);
         var ultimatePdfLib = new ExternalLogic.UltimatePDF_ExternalLogic();
         var viewport = new ExternalLogic.Structures.Viewport { Width = 1366, Height = 768 };
         var environment = new ExternalLogic.Structures.Environment {
-            BaseURL = "professionalservices-dev.outsystems.app",
+            BaseURL = uri.Host,
             Locale = "en",
             Timezone = "Europe/Lisbon"
         };
@@ -52,14 +86,7 @@ public class Function {
             attachFilesLogs: false,
             out byte[] logsZipFile);
 
-        var timestamp = DateTime.Now.ToString("yyyyMMddTHHmmssfff");
-        Task[] tasks = {
-            Task.Run(() => PutS3Object(bucket, $"pdf_{timestamp}.pdf", pdf)),
-            Task.Run(() => PutS3Object(bucket, $"logs_{timestamp}.zip", logsZipFile))
-        };
-        Task.WaitAll(tasks);
-
-        return $"Got a pdf with size {((pdf.Length / 1024.0f) / 1024.0f)} Mb and the logs as {((logsZipFile.Length / 1024.0f) / 1024.0f)} Mb";
+        return (pdf, logsZipFile);
     }
 
     public static async Task<bool> PutS3Object(string bucket, string key, byte[] content) {
